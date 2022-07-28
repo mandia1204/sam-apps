62e5642801a251ccb0850903859443ddb2f6974b
Marvin Andia <mandia1204@gmail.com>
2022-07-27 19:32:38 -0500

    running tests

--- get-profile/tests/unit/test-handler.js
+++ get-profile/tests/unit/test-handler.js
@@ -9,14 +9,17 @@ describe('Tests index', function () {
     it('verifies successful response', async () => {
         const result = await app.lambdaHandler(event, context)
 
-        expect(result).to.be.an('object');
         expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
 
-        let response = JSON.parse(result.body);
+        let body = JSON.parse(result.body);
 
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello world");
-        // expect(response.location).to.be.an("string");
+        const expected = {
+            userName : 'testUser',
+            age: 25,
+            logo: 'image1.jpg',
+            preferences: []
+        };
+        expect(body).to.be.an('object');
+        expect(body).to.deep.equal(expected);
     });
 });
